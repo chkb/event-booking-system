6a1ca26a0dca853aa1b5b642b6c0bc45db58f95b
Chakib Grube Benhaddou <chakib.benhaddou@maersk.com>
2018-10-05 22:55:56 +0200

    fix prevuios event on employee site

--- src/app/event-history/event-history.component.ts
+++ src/app/event-history/event-history.component.ts
@@ -5,7 +5,7 @@ import { AngularFirestore } from 'angularfire2/firestore';
 import * as moment from 'moment';
 
 import { LoginProviderService } from '../core/login-provider.service';
-import { EventHistory } from '../shared/event';
+import { EventHistory, EventHistoryViewModel, Booked } from '../shared/event';
 
 
 @Component({
@@ -29,7 +29,7 @@ export class EventHistoryComponent implements OnInit {
         private router: Router,
         private lps: LoginProviderService
     ) {
-        afs.firestore.settings({ timestampsInSnapshots: true });        
+        afs.firestore.settings({ timestampsInSnapshots: true });
     }
 
     ngOnInit() {
@@ -40,21 +40,24 @@ export class EventHistoryComponent implements OnInit {
 
     getEmployeeData(): void {
         this.list = [];
-        const year = moment().year().toString();
-        this.afs.collection('event-history').ref.doc(this.employeeId).collection(year).get().then(querySnapshot => {
+        this.afs.collection('events').ref.get().then(querySnapshot => {
             querySnapshot.forEach(doc => {
-                const eh = new EventHistory();
-                eh.comments = doc.data()['comments'];
-                eh.employeeName = doc.data()['employeeName'];
-                eh.employeeUid = doc.data()['employeeUid'];
-                eh.eventName = doc.data()['eventName'];
-                eh.eventUid = doc.data()['eventUid'];
-                eh.date = doc.data()['date'];
-                this.list.push(eh);
+                const bookedList = doc.data()['booked'];
+                bookedList.forEach((booked: Booked) => {
+                    const eh = new EventHistoryViewModel();
+                    if (booked.uid === this.employeeId) {
+                        eh.comments = booked.comment;
+                        eh.employeeName = booked.displayName;
+                        eh.employeeUid = booked.uid;
+                        eh.eventUid = doc.id;
+                        eh.event = doc.data();
+                        this.list.push(eh);
+                    }
+                });
             });
-            this.list.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-            this.dataSource = new MatTableDataSource(this.list);
         });
+        this.list.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+        this.dataSource = new MatTableDataSource(this.list);
     }
 
     gotoEvent(id: string): void {

38dc86f50520bb78fe826fb9def30952ab340e9b
Chakib Benhaddou <benhaddou.chakib@gmail.com>
2018-11-09 20:34:00 +0100

    update login page with button

--- src/app/event/edit/edit.component.spec.ts
+++ src/app/event/edit/edit.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EditComponent } from './edit.component';
+import { EventEditComponent } from './edit.component';
 
-describe('EditComponent', () => {
-    let component: EditComponent;
-    let fixture: ComponentFixture<EditComponent>;
+describe('EventEditComponent', () => {
+    let component: EventEditComponent;
+    let fixture: ComponentFixture<EventEditComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [EditComponent]
+            declarations: [EventEditComponent]
         })
             .compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(EditComponent);
+        fixture = TestBed.createComponent(EventEditComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
